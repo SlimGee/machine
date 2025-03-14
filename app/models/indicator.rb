f3cfb8863f8652bb515fa86660ab857be9465c6b
Given Ncube <givenyslim12@gmail.com>
2025-03-14 10:06:54 +0200

    wip

--- app/models/indicator.rb
+++ app/models/indicator.rb
@@ -3,6 +3,6 @@ class Indicator < ApplicationRecord
   has_many :event_indicators
   has_many :events, through: :event_indicators
 
-  validates :type, :value, presence: true
+  validates :indicator_type, :value, presence: true
   validates :confidence, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }, allow_nil: true
 end

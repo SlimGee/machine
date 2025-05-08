8e980857f4ab1ca8a6d1fdc9c5a083a885999dfb
Given Ncube <givenyslim12@gmail.com>
2025-05-08 06:26:48 +0200

    feat: added the ability to predict targets

--- app/jobs/create_model_embeddings_job.rb
+++ app/jobs/create_model_embeddings_job.rb
@@ -1,6 +1,6 @@
 class CreateModelEmbeddingsJob < ApplicationJob
   queue_as :default
-  limits_concurrency to: 1, key: :create_model_embeddings_job
+  limits_concurrency to: 1, key: :create_model_embeddings_job, duration: 5.minutes
 
   def perform(model)
     model.upsert_to_vectorsearch

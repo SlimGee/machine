170a5c1423a8530e97ac3d61df4a2ad96adb2bfa
Given Ncube <givenyslim12@gmail.com>
2025-05-08 11:22:46 +0200

    wip: dashboard

--- app/controllers/app/home_controller.rb
+++ app/controllers/app/home_controller.rb
@@ -1,4 +1,8 @@
 class App::HomeController < App::ApplicationController
   def index
+    @total_hosts = Host.count
+    @total_threat_actors = ThreatActor.count
+    @total_predictions = Prediction.count
+    @total_events = Event.count
   end
 end
